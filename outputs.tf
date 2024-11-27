44d2be36b6226eb3d986c77720b4d89ca9162b5c
Magnus Longva <magnus@longva.email>
2024-11-27 08:36:02 +0100

    add output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "cognitive_account_resource_id" {
   value = azapi_resource.cognitive_account.id
+  description = "The resource ID of the created cognitive account"
 }
 
 output "identity" {
   value = azapi_resource.cognitive_account.identity
+  description = "The system-assigned managed identity of the created cognitive account"
 }
